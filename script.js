fb62ee4eb0deb53e73794423e271d603ccacb64f
gizedwards <gizedwards@gmail.com>
2022-08-05 03:13:19 +0100

    added the weighing function

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const draggable = document.querySelectorAll('.draggable');
 const containers = document.querySelectorAll('.container');
 
-
 //makes things draggable
 draggable.forEach(draggable => {
     draggable.addEventListener('dragstart', () =>{
@@ -13,14 +12,6 @@ draggable.forEach(draggable => {
     })
 
 })
-
-//appends dropped to class
-draggable.forEach(draggable => {
-    draggable.addEventListener('dragend',() => {
-        draggable.classList += (' dropped')
-    }
-    )}
-)
 
 containers.forEach(container => {
     container.addEventListener('dragover', e => {
@@ -49,101 +40,77 @@ function getDragAfterElement(container, y) {
         }, { offset: Number.NEGATIVE_INFINITY }).element
 }
 
-//turned the id of each islander into their own let
-
-let Amy = document.getElementById('#Amy');
-let Boyle = document.getElementById('#Boyle');
-let Doug = document.getElementById('#Doug');
-let Gina = document.getElementById('#Gina');
-let Hitchcock = document.getElementById('#Hitchcock');
-let Holt = document.getElementById('#Holt');
-let Jake = document.getElementById('#Jake');
-let Kevin = document.getElementById('#Kevin');
-let Pimento = document.getElementById('#Pimento');
-let Rosa = document.getElementById('#Rosa');
-let Scully = document.getElementById('#Scully');
-let Terry = document.getElementById('#Terry');
-
-
-//turned the islander let into a number, all equal. 
-Amy = 10;
-Boyle = 10;
-Doug = 10;
-Gina = 10;
-Hitchcock = 10;
-Holt = 10;
-Jake = 10;
-Kevin = 10;
-Pimento = 10;
-Rosa = 10;
-Scully = 10;
-Terry = 10;
-
-//created a random number between 1-2, 
-//if it was 1 then it ='-10'
-//if it was anything but 1 ='10'
-
+//Create a random number either -1, or +1
 let randomNumber = Math.floor(Math.random() *2) +1;
 if (randomNumber == 1) {
-    randomNumber = -10;
-} else (randomNumber = 10);
+    randomNumber = -1;
+} else (randomNumber = 1);
 
-//need to choose a random person.
+//Choose a random person.
 let listOfPeople = ['Amy', 'Boyle', 'Doug', 'Gina', 'Hitchcock', 'Holt', 'Jake', 'Kevin', 'Pimento', 'Rosa', 'Scully', 'Terry'];
 let randomPerson = listOfPeople[Math.floor(Math.random() * listOfPeople.length)];
-let oddOneOut = randomPerson
 
-//out of the list of random people I assigned the randomnumber to their value
+function creatingTheCulprit() {
 if (randomPerson == 'Amy') {
-    randomPerson = Amy + randomNumber
+    culprit = Amy.classList.add('culprit');
 }
-else if (randomPerson == 'Boyle') {
-    randomPerson = Boyle + randomNumber
+if (randomPerson == 'Boyle') {
+    culprit = Boyle.classList.add('culprit');
 }
-else if (randomPerson == 'Doug') {
-    randomPerson = Doug + randomNumber
+if (randomPerson == 'Doug') {
+    culprit = Doug.classList.add('culprit');
 }
-else if (randomPerson == 'Gina') {
-    randomPerson = Gina + randomNumber
+if (randomPerson == 'Gina') {
+    culprit = Gina.classList.add('culprit');
 }
-else if (randomPerson == 'Hitchcock') {
-    randomPerson = Hitchcock + randomNumber
+if (randomPerson == 'Hitchcock') {
+    culprit = Hitchcock.classList.add('culprit');
 }
-else if (randomPerson == 'Holt') {
-    randomPerson = Holt + randomNumber
+if (randomPerson == 'Holt') {
+    culprit = Holt.classList.add('culprit');
 }
-else if (randomPerson == 'Jake') {
-    randomPerson = Jake + randomNumber
+if (randomPerson == 'Jake') {
+    culprit = Jake.classList.add('culprit');
 }
-else if (randomPerson == 'Kevin') {
-    randomPerson = Kevin + randomNumber
+if (randomPerson == 'Kevin') {
+    culprit = Kevin.classList.add('culprit');
 }
-else if (randomPerson == 'Pimento') {
-    randomPerson = Pimento + randomNumber
+if (randomPerson == 'Pimento') {
+    culprit = Pimento.classList.add('culprit');
 }
-else if (randomPerson == 'Rosa') {
-    randomPerson = Rosa + randomNumber
+if (randomPerson == 'Rosa') {
+    culprit = Rosa.classList.add('culprit');
 }
-else if (randomPerson == 'Scully') {
-    randomPerson = Scully + randomNumber
+if (randomPerson == 'Scully') {
+    culprit = Scully.classList.add('culprit');
 }
-else if (randomPerson == 'Terry') {
-    randomPerson = Terry + randomNumber
+if (randomPerson == 'Terry') {
+    culprit = Terry.classList.add('culprit');
+}
 }
 
 console.log(randomPerson);
-console.log(oddOneOut);
+console.log(randomNumber);
 
+function clickToWeigh() {
+    creatingTheCulprit();
+    const leftside = document.getElementById('leftside');
+    const rightside = document.getElementById('rightside');
+    let leftcount = leftside.children.length;
+    let rightcount = rightside.children.length;
+    
+    if (leftside.querySelector('.culprit')) {
+        leftcount = leftcount + randomNumber;
+    }
+    else if (rightside.querySelector('.culprit')) {
+        rightcount = rightcount + randomNumber;
+    }
 
-// need to add people's weight onto the see saw
-//need to add event listener to button
-
-// poss add an apend to the class
-
-//makes the button clickable
-function clickbutton () {
-    console.log();
-   }
-
-//this could be the answer to the problem id stuff.
-//https://stackoverflow.com/questions/71311826/how-to-check-which-element-is-dropped-in-which-drag-zone-in-javascript
+    if (leftcount == rightcount){
+        console.log('equal weight')
+    } else if (leftcount > rightcount) {
+        console.log('left heavier')
+    } else if (leftcount < rightcount) {
+        console.log('right heavier')
+    }
+}
